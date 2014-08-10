e353b945dd318c50a60da19f7e1a44893c094a44
Ariel Rodriguez <ariel@volonbolon.net>
2014-08-10 18:06:48 -0300

    Add List View Controller

---
+++ MediaSocial/MediaSocial/FWKListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FWKListTableViewController.h
+//  MediaSocial
+//
+//  Created by Ariel Rodriguez on 8/10/14.
+//  Copyright (c) 2014 Ariel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FWKListTableViewController : UITableViewController
+
+@end
