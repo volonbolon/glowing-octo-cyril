a6ef5edf26aa1ee211be78956e00540cf029ce56
Ariel Rodriguez <ariel@volonbolon.net>
2014-08-10 18:07:29 -0300

    Add FWKRecorderViewController

---
+++ MediaSocial/MediaSocial/FWKRecorderViewController.h
@@ -0,0 +1,13 @@
+//
+//  FWKRecorderViewController.h
+//  MediaSocial
+//
+//  Created by Ariel Rodriguez on 8/10/14.
+//  Copyright (c) 2014 Ariel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FWKRecorderViewController : UIViewController
+
+@end
